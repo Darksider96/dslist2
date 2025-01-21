c989b6e6088be769bb5f22cddfb84f3520d38af1
Darksider96 <115590255+Darksider96@users.noreply.github.com>
2025-01-21 20:02:09 -0300

    Consulto SQL, Projection, busca de games por lista

--- src/main/java/com/darksider/dslist/services/GameService.java
+++ src/main/java/com/darksider/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.darksider.dslist.dto.GameDTO;
 import com.darksider.dslist.dto.GameMinDTO;
 import com.darksider.dslist.entities.Game;
+import com.darksider.dslist.projections.GameMinProjection;
 import com.darksider.dslist.repositories.GameRepository;
 
 @Service
@@ -21,11 +22,19 @@ public class GameService {
 		Game result = gameRepository.findById(id).get();
 		return new GameDTO(result);
 	}
+	
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findALL() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
 	}
 
 }
